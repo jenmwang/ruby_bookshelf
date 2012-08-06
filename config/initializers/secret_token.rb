d5db3a35c40c320456a132422490d510067edd64
Jen <user@server>
2012-08-05 17:41:28 -0700

    first commit not fun

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyBookshelf::Application.config.secret_token = 'f36453842c64b90bbe0027a5e247241ffdb50b340ce97557ffb95159a4fcb8c52831222673f8236f622d3fc7b2ca5f00408210bc113194e7ce401f21e250058f'
